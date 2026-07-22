d66c379ac7c5e6eb47d21c46cd1c61eade3e4a69
leetaeyong <tkwk651@naver.com>
2026-07-22 17:36:06 +0900

    새로 커밋

---
+++ ch1/basic.py
@@ -0,0 +1,14 @@
+a = 123
+print(a)
+
+a= -178
+print(a)
+
+multiline = """Life is too short,\n You need python"""
+print(multiline)
+
+multiline = """
+Life is too short,
+You need python
+"""
+print(multiline)
